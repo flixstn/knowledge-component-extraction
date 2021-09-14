f47d73a57cdc4eeb198ab05bdc16a539266dc292
flix <felixstein1508@aol.com>
2021-09-14 13:30:50 +0200

    Add parser trait

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,3 +1,9 @@
-pub(crate) mod parser;
+pub(crate) mod cjparser;
 pub(crate) mod pyparser;
 pub(crate) mod knowledge_component;
+
+use std::error::Error;
+
+pub trait Parser {
+    fn parse(&mut self, file: &str, time_code: i32) -> Result<(), Box<dyn Error>>;
+}
