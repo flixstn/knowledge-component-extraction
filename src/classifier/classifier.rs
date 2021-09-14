c6229f2ec9274024dc2490ee0a00ff6ba9270007
flix <felixstein1508@aol.com>
2021-09-14 13:34:09 +0200

    Add language classification

---
+++ src/classifier/classifier.rs
@@ -0,0 +1,38 @@
+use std::{process::Command, str::from_utf8};
+
+pub struct LanguageClassifier;
+
+#[derive(Debug)]
+pub enum ProgrammingLanguage {
+    C,
+    Cpp,
+    Java,
+    Python,
+}
+
+impl LanguageClassifier {
+    pub fn classify<S: Into<String>>(val: S) -> Option<ProgrammingLanguage> {
+        let lang = val.into().to_lowercase();
+
+        if lang.contains("c++") || lang.contains("cpp") {
+            return Some(ProgrammingLanguage::Cpp);
+        } else if lang.contains("java") {
+            return Some(ProgrammingLanguage::Java);
+        } else if lang.contains("python") {
+            return Some(ProgrammingLanguage::Python);
+        } else if lang.contains("c ") {
+            return Some(ProgrammingLanguage::C);
+        }
+
+        return None
+    }
+
+    pub fn classify_ml(val: &str) {
+        let mut output = Command::new("./src/classifier/classifier").args(&[&format!("{}", val)]).output().unwrap();
+        let language = from_utf8(&output.stdout).unwrap();
+
+        println!("{}", language);
+    }
+
+
+}
